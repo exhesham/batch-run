f22f4b875a9b1048ed726d10944d28fc051d2e0e
Hesham Yassin <exhesham@gmail.com>
2021-12-11 12:06:55 +0200

    solution 2

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 const chunkSize = 3;
 const numberOfTasks = 10;
-let tasksSleepInput: Array<{delay: number, index: string }> = Array(numberOfTasks).fill(30).map(v => ({delay: v, index: '' }));
+let tasksSleepInput: Array<{delay: number, index: string }>;
 
 // let's give index to each task
 Object.keys(tasksSleepInput).forEach(k => tasksSleepInput[Number.parseInt(k)].index = k);
@@ -16,6 +16,7 @@ let taskExecutor = async (task: {delay: number, index: string}) => {
 };
 
 async function solution1() {
+    tasksSleepInput = Array(numberOfTasks).fill(30).map(v => ({delay: v, index: '' }));
     for(let i = 0; i < tasksSleepInput.length; i+= chunkSize) {
         let batch = tasksSleepInput.slice(i, i + chunkSize);
         console.log(`* start running batch ${i}`);
@@ -24,4 +25,22 @@ async function solution1() {
     }
 }
 
-solution1();
+
+
+
+async function solution2() {
+    let tasksQueue = Array(numberOfTasks).fill(Math.round(Math.random() * 30) + 1)
+    .map(v => (() => taskExecutor({delay: v, index: '' })));
+    let worker = async (index: number) => () => {
+        
+        while(tasksQueue.length > 0) {
+            console.log(`worker ${index} handles task`);
+            let task = delete tasksQueue[0];
+            await task();
+            console.log(`worker ${index} finished handling task`);
+        }
+    };
+    await Promise.all([worker(1), worker(2), worker(3), worker(4)]);
+}
+
+solution2();
